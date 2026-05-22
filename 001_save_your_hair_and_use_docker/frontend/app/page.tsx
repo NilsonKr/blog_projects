8f1b7d1ed72b21872a2bd216e91e186af34b7e7f
Nilson Diaz <nuotrok444@gmail.com>
2026-05-21 23:58:45 -0500

    [001_front] Setup initial styles

--- 001_save_your_hair_and_use_docker/frontend/app/page.tsx
+++ 001_save_your_hair_and_use_docker/frontend/app/page.tsx
@@ -8,9 +8,12 @@ export default async function Home() {
   const subjects = await fetchSubjects()
 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="bg-[url('/background.svg')] bg-size-[250px] bg-default flex flex-col flex-1 items-center justify-center font-sans dark:bg-black">
+      <main className="py-32 px-16">
         <LobbyChat />
+        <h2 className="font-impact">
+          WARNING
+        </h2>
       </main>
     </div>
   );
