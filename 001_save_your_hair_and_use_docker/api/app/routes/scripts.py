1c08780b7fdf97ea63f59254a0bef69bedf96205
Nilson Diaz <nuotrok444@gmail.com>
2026-06-16 00:58:48 -0500

    Update images url at script endpoint

--- 001_save_your_hair_and_use_docker/api/app/routes/scripts.py
+++ 001_save_your_hair_and_use_docker/api/app/routes/scripts.py
@@ -10,8 +10,8 @@ router = APIRouter(prefix="/scripts", tags=["scripts"])
 
 @router.post("/", status_code=201, response_model=list[SubjectRead])
 async def create_sample_subjects(session: Session_dep):
-    subject_1 = Subject(name="Ppepe carlo", pic="https://example.com/subject1.jpg")
-    subject_2 = Subject(name="SSDSUD", pic="https://example.com/subject2.jpg")
+    subject_1 = Subject(name="Omali", pic="https://pbs.twimg.com/media/GXZMabjbwAIu4jr.jpg")
+    subject_2 = Subject(name="Pepe Carlo", pic="https://i.ytimg.com/vi/lwdAgaKd2_I/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDRsn-CQImDXh_aNCkzh5uq19nw8w")
     
     session.add(subject_1)
     session.add(subject_2)  
